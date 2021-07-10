95ac1b0757aa44bd461097a00eae059cfa5cdb8a
이름 <wltnwkd244@naver.com>
2021-07-10 11:06:05 +0900

    add logined page

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 const { Template } = require('ejs')
 const express=require('express')
+const { session } = require('passport')
 const path=require('path')
 
 const router=express.Router()
 const User=require('../models/user')
+const passport = require('../passport')
 
 router.get('/',(req,res,next) => {
-   if(req.isAuthenticated()) {
-       let list=Template.list(req.list);
-       let html=Template.HTML(title,list,
-        `1<div id="logined">
-        <h3 class="text-center">환영합니다!</h3>
-        <h4 class="username">${req.user}</h4>
-        <div id="logout-link" class="text-right">
-            <a href="/" class="text-info">logout</a>
-    </div>`                    
-                 
-        )
-   }
-    res.render('main')
+   
+    res.render('main', {
+        user: req.user
+    })
 
 })
 
